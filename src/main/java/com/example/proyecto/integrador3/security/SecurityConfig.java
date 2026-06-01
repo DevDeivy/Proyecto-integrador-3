b1c957c7ee60fc784b1d86f8a2ffa1c465c966ac
Daniel Andres Cabana Trejos <danielcabana0727@gmail.com>
2026-06-01 13:59:20 -0500

    feat: agregar https://ciudadreporta.vercel.app a CORS permitidos

--- src/main/java/com/example/proyecto/integrador3/security/SecurityConfig.java
+++ src/main/java/com/example/proyecto/integrador3/security/SecurityConfig.java
@@ -71,7 +71,8 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:8080",
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "https://ciudadreporta.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
