c69369415eb7977ddbab1ae3ead48918d11a6fa6
Daniel Andres Cabana Trejos <danielcabana0727@gmail.com>
2026-06-10 16:36:52 -0500

    fix: agregar https://ciudadreporta-flame.vercel.app a CORS

--- src/main/java/com/example/proyecto/integrador3/security/CorsConfig.java
+++ src/main/java/com/example/proyecto/integrador3/security/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig {
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "https://ciudadreporta.vercel.app"
+                "https://ciudadreporta.vercel.app",
+                "https://ciudadreporta-flame.vercel.app"
         ));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("*"));
